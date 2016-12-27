66c054974c4bc5e56c9f5dd167f6c0facd31e7b9
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-27 13:56:01 +0200

    fixed up pycharm errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pipegzip',
-    version='0.0.1',
+    version='0.0.2',
     description='faster read of gzip files using a pipe to zcat(1)',
     long_description='pipegzip helps you read gzipped files faster by using a subprocess',
     url='https://veltzer.github.io/pipegzip',
