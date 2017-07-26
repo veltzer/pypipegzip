efaacc836eff304f36b272eb98e6fc84cbb943c1
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-26 15:26:13 +0300

    support 2.7 start

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
-
 setuptools.setup(
     name='pypipegzip',
-    version='0.0.4',
+    version='0.0.5',
     description='faster read of gzip files using a pipe to zcat(1)',
     long_description='pypipegzip helps you read gzipped files faster by using a subprocess',
     url='https://veltzer.github.io/pypipegzip',
@@ -17,6 +13,7 @@ setuptools.setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
     ],
     keywords='pipe pipeline gzip read speed pypipegzip',
     package_dir={'': 'src'},
