a95533b2b9d1ba760f327b7115d24af458e0c4d2
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-27 19:53:00 +0200

    webpage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.16',
     description='faster read of gzip files using a pipe to zcat(1)',
     long_description='pypipegzip helps you read gzipped files faster by using a subprocess',
-    url='https://github.com/veltzer/pypipegzip',
+    url='https://veltzer.github.io/pypipegzip',
     download_url='https://github.com/veltzer/pypipegzip',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
