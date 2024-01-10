5ab2899249e26d524c98ef9b13bd4cd210d10a28
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 18:18:06 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -1,15 +1,14 @@
 from typing import List
 
 
-config_requires: List[str] = [
-    "pyclassifiers",
-]
 dev_requires: List[str] = [
     "pypitools",
 ]
+config_requires: List[str] = [
+    "pyclassifiers",
+]
 install_requires: List[str] = []
 make_requires: List[str] = [
-    "pyclassifiers",
     "pymakehelper",
     "pydmt",
 ]
