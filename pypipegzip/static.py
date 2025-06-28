768a732f985eb2805555de6950910e5e2ca96fe4
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:21 +0300

    more

---
+++ pypipegzip/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.24"
+DESCRIPTION = "faster read of gzip files using a pipe to zcat(1)"
+APP_NAME = "pypipegzip"
+LOGGER_NAME = "pypipegzip"
