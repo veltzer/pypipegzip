6338e18a27300a93a4b3b09c1c58788e2f6967bb
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,10 @@
+NAME = "pypipegzip"
+DESCRIPTION_SHORT = "faster read of gzip files using a pipe to zcat(1)"
+KEYWORDS = {
+    "pipe",
+    "pipeline",
+    "gzip",
+    "read",
+    "speed",
+    "pypipegzip",
+}
